11ec28e01eefe6b91c6db09e4d1b821ef5705e01
Kun-il <sun237dksl@gmail.com>
2022-11-01 18:15:50 +0900

    Complete my django project

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list')     # 기본 root-url, views = 일종의 controller
+    path('', views.post_list, name='post_list'),     # 기본 root-url, views = 일종의 controller
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new', views.post_new, name='post_new'),     # 게시물 등록 처리
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 
-# Create your views here.
+from blog.models import Post
+from django.utils import timezone
+
+from blog.templates.blog.forms import PostForm
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    # posts 라는 쿼리셋 선언하면서 결과 받아옴
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+# 게시물을 작성 폼을 보여주는 핸들러(뷰 메소드) 추가
+# 폼을 불러올때 submit할떄, 둘 다 이 뷰어를 사용
+# 이때 request에는 사용자의 요청 전체 내용이 들어있음
+def post_new(request):
+    if request.method == "POST":
+        # request.POST : 화면에서 사용자가 입력한 내용들이 담겨있음
+        form = PostForm(request.POST)
+        if form.is_valid():     # 폼이 유효하다면
+            # 임시 저장하여 post 객체를 리턴받는다.
+            post = form.save(commit=False)
+            post.author = request.user
+            # 실제 저장을 위해 작성일시를 설정한다.
+            post.published_date = timezone.now()
+            post.save()     # 데이터를 실제로 저장한다.
+            return redirect('post_detail', pk=post.pk)
+
+    else:
+        form = PostForm()
+    # return 문은 else: 와 같은 레벨이어야 한다.
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    # 수정하고자 하는 글의 게시물에 해당하는 Post 모델 인스턴스(instance)를 데이터베이스에서 가져옴
+    post = get_object_or_404(Post, pk=pk)
+
+    # 수정 화면의 내용을 저장할 때 처리
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:   # 처음 수정폼을 띄워줄 때 처리
+        form = PostForm(instance=post)
+
+    return render(request, 'blog/post_edit.html', {'form': form})
